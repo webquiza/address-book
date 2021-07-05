2b4bf1bb9b21db3d2942e3a86e5f7fc330c74e70
Carlos Urquiza <webquiza@gmail.com>
2021-07-05 13:35:08 -0700

    add AddressBook constructor to contain list of Contact objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// Business Logic for AddressBook
+function AddressBook() {
+  this.contacts = {};
+}
+
+// Business Logic for Contacts
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
